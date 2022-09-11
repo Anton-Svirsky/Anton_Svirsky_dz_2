51f742049ff556e8748dd87d00857c371660cf64
Anton <anton_svirsky@mail.ru>
2022-09-11 15:46:54 +0300

    task_3 added

--- task_2_2.py
+++ task_2_2.py
@@ -3,3 +3,10 @@
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
+number = int(input('Enter a number: '))
+factorial = 1
+temp_list = []
+for i in range(1, number + 1):
+    temp_list.append(factorial * i)
+    factorial = factorial * i
+print(f'пусть N = {number}, тогда {temp_list}')
